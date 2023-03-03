88fb632ed6ab00dba40598f84390cd161f81aca6
Liuming9124 <=>
2023-03-03 13:08:30 +0800

    init

--- web/route/modules/db.js
+++ web/route/modules/db.js
@@ -1,6 +1,5 @@
 const neo4j = require('neo4j-driver')
 
-const db = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'neo4j'))
-
+const db = neo4j.driver('neo4j+s://4c593094.databases.neo4j.io', neo4j.auth.basic('neo4j', 'O2dDsdj9AJIiKFq9rKsHd8iZAxtdo0Am7UpmMqhMhcU'))
 
 module.exports = db;
