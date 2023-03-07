8623571ea925e7dd71b17979d6c4c895042d32c7
Liuming9124 <=>
2023-03-08 01:57:28 +0800

    db-connection

--- web/route/modules/db.js
+++ web/route/modules/db.js
@@ -1,5 +1,11 @@
 const neo4j = require('neo4j-driver')
 
-const db = neo4j.driver('neo4j+s://4c593094.databases.neo4j.io', neo4j.auth.basic('neo4j', 'O2dDsdj9AJIiKFq9rKsHd8iZAxtdo0Am7UpmMqhMhcU'))
+
+const uri = 'bolt://125.229.139.63:7687';
+const user = 'neo4j';
+const password = 'O2dDsdj9AJIiKFq9rKsHd8iZAxtdo0Am7UpmMqhMhcU';
+
+
+const db = neo4j.driver(uri, neo4j.auth.basic(user, password))
 
 module.exports = db;
