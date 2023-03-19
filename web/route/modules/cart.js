256a41e605e2bd78ef416312471a2a4a01be1c45
Liuming9124 <=>
2023-03-20 01:13:37 +0800

    cartDelete

--- web/route/modules/cart.js
+++ web/route/modules/cart.js
@@ -7,12 +7,18 @@ const cartCtl = require('../../controllers/cartctl')
 
 router.get('/', cartCtl.cartPage)
 
-// router.get('/cart', (req, res) => {
-//     // 显示购物车中的商品列表
-//     const cart = req.session.cart || [];
-//     res.send(cart);
-// });
-
+router.get('/delcart/:id', (req, res) => {
+    // delete the index at id of cart[] array
+    const index = req.params.id;
+    // console.log('index='+index) 
+    
+    // chech if index is valid
+    if (index >= 0 && index < req.session.cart.length) {
+        req.session.cart.splice(index, 1);  // delete cart[id] by function array.splice()
+    }
+    
+    res.redirect('/cart')
+});
 
 
 module.exports = router
