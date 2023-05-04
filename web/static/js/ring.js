e15f3f4e5608005f79ee8e9232e10e8387510522
Liuming9124 <=>
2023-05-04 20:04:05 +0800

    ringUpdateFunction

--- web/static/js/ring.js
+++ web/static/js/ring.js
@@ -16,58 +16,62 @@ function fetchData() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 // success response returned from server
-                var data = xhr.response;
-                // for (let i=0;i<data.length();i++){
-                    var html = '';
-                    if (xhr.response[i]==0){
-                        html = `
-                        <!-- Feature -->
-                        <section>
-                            <!-- 有人按鈴 -->
-                                <a>&nbsp;</a><br><br>
-                            <div class="tablestyle3">第 {i} 桌</div><br>
-                                <a href="#"><h5>
-                                    if (i%2==0){ %>
-                                        <div class=" warning-text">尚有未送餐</div>
-                                    } %>	
-                                    if (i%2==1){ %>
-                                        訂單皆已送餐
-                                    } %>
-                                </h5></a><br>
-                                <button type="button" class="btn">送餐</button></button>     
+                var data = (xhr.response);
+                var i=0
+                for (i=0;i<data.length;i++){
+                    var html=''
+                    if (data[i]==0){
+                        html=`
+                            <!-- Feature -->
+                            <section>
+                                <!-- 有人按鈴 -->
+                                    <a>&nbsp;</a><br><br>
+                                <div class="tablestyle3">第 ${i+1} 桌</div><br>
+                                    <a href="#"><h5>
+                                        <!-- if (i%2==0){  -->
+                                            <div class=" warning-text" href="#" data-toggle="modal" data-target="#Modalrecord">尚有未送餐</div>  	<!-- Modal2 -->
+                                        <!--  }	 -->	
+                                        <!--if (i%2==1){ -->
+                                            訂單皆已送餐
+                                        <!-- } -->
+                                    </h5></a><br>
+                                    <button type="button" class="btn">送餐</button></button>     
+                            </section>
+                        `
+                    }
+                    else{
+                        html=`
+                            <!-- Feature -->
+                            <section>
+                                <!-- 沒人按鈴 -->
+                                    <a class="warning-text" data-toggle="modal" data-target=".bs-example-modal-sm">
+                                        <span class="glyphicon glyphicon-bell" aria-hidden="true"></span>
+                                    </a><br><br>
+                                <div class="tablestyle3">第 ${i+1} 桌</div><br>
+                                    <a href="#"><h5>
+                                        <!-- if (i%2==0){  -->
+                                            <div class=" warning-text" href="#" data-toggle="modal" data-target="#Modalrecord">尚有未送餐</div>  	<!-- Modal2 -->
+                                        <!--  }	 -->	
+                                        <!--if (i%2==1){ -->
+                                            訂單皆已送餐
+                                        <!-- } -->
+                                    </h5></a><br>
+                                    <button type="button" class="btn">送餐</button></button>     
                             </section>
                         `
 
                     }
-                    else{
-                        html = `
-                        <!-- Feature -->
-                        <section>
-                            <!-- 沒人按鈴 -->
-                            <a class="warning-text" data-toggle="modal" data-target=".bs-example-modal-sm">
-                                <span class="glyphicon glyphicon-bell" aria-hidden="true"></span>
-                            </a><br><br>
-                            <div class="tablestyle3">第 {i} 桌</div><br>
-                                <a href="#"><h5>
-                                    if (i%2==0){ %>
-                                        <div class=" warning-text">尚有未送餐</div>
-                                    } %>	
-                                    if (i%2==1){ %>
-                                        訂單皆已送餐
-                                    } %>
-                                </h5></a><br>
-                                <button type="button" class="btn">送餐</button></button>     
-                            
-                        </section>
-                    `
-                    }
-                    
+
                     // update html content
-                    var element = document.getElementById(`table0`);
+                    var element = document.getElementById(`table${i}`);
                     // element.outerHTML = html;  
-                    element.innerHTML = '<b>哈哈哈 沒用</b>'
+                    element.innerHTML = html
+
                     
-                // }
+                }
+                // var element = document.getElementById(`table0`);
+                //     // element.outerHTML = html;  
+                // element.innerHTML = data
  
             } else {
                 // response returned with non-success status
@@ -78,4 +82,4 @@ function fetchData() {
 }
 
 // set interval to call api every 3 seconds
-setInterval(fetchData, 3000);
+setInterval(fetchData, 1000);
