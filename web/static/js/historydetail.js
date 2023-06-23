73d14895eaa2d3ab60315accbbd39e5d8cdd6fda
“timyfanggit config --global user.name “timy901227@gmail.com <timy901227@gmail.com>
2023-06-23 18:24:12 +0800

    history(bug)

--- web/static/js/historydetail.js
+++ web/static/js/historydetail.js
@@ -3,8 +3,9 @@ function updateDisplayRange(startDate, startTime, endDate, endTime, displayRange
     const startTimeValue = startTime.value;
     const endValue = endDate.value;
     const endTimeValue = endTime.value;
-  
+      // 定義時間變數
     displayRange.textContent = `目前顯示時間範圍：${startValue} ${startTimeValue} ~ ${endValue} ${endTimeValue}`;
+     // 把時間範圍顯示在畫面上
   }
   
   const form = document.getElementById('serarchOrderBtn');
@@ -16,7 +17,7 @@ function updateDisplayRange(startDate, startTime, endDate, endTime, displayRange
   
   form.addEventListener('submit', function(event) {
     updateDisplayRange(startDate, startTime, endDate, endTime, displayRange);
-    
+   //執行函式
   });
 function historydetail(oid) {
     // 在這裡執行您的代碼，根據ID區分不同的元素
