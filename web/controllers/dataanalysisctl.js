a1798bcf92034d3270000a19acebee06c8011bbd
“timyfanggit config --global user.name “timy901227@gmail.com <timy901227@gmail.com>
2023-08-21 21:15:26 +0800

    dataana

--- web/controllers/dataanalysisctl.js
+++ web/controllers/dataanalysisctl.js
@@ -1,4 +1,6 @@
 const db = require("../route/modules/db");
+const funCtl = require("../route/modules/fun");
+
 // var session = db.session()
 
 
@@ -42,6 +44,22 @@ const dataanalysisController = {
                     res.send(`{"status":"${error}"}}`)
                 })
                 .then(async () => {
+                    var menu = []
+                    menu = JSON.parse(await funCtl.getMenu())
+                    //console.log('menu:',menu)
+                    var food = {}; // 創建一個空物件來存儲菜單數據
+
+                    for (var i = 0; i < menu.length; i++) {
+                        var category = menu[i].name; // 獲取類別名稱
+                        var items = menu[i].items;   // 獲取食物項目數組
+                        food[category] = {}; // 在food物件中創建一個空物件來存儲當前類別的食物項目
+                        for (var j = 0; j < items.length; j++) {
+                          food[category][j + 1] = { name: items[j].name, count: 0 }; // 將食物項目添加到當前類別下，注意這裡的 j + 1 作為食物項目的編號
+                        }
+                      }
+                      
+                      console.log('menu:',food);
+
                     // console.log(JSON.stringify(forder))   //上一層的所有訂單結果  
                     // 使用第二個db連接查詢下一層訂單明細
                     var session2 = db.session()
@@ -77,6 +95,16 @@ const dataanalysisController = {
                 })
                 .catch(error => {
                     console.log(error)
+                })
+                .then(() => {
+                    var session3 = db.session()
+                    try {
+                        const result3 =session3.run(`MATCH (n:type) RETURN n LIMIT 25`);
+                        console.log(result3);
+                    } catch (error) {
+                        console.error(error);
+                    }    
+                    
                 })
                 .then(() => {
                     session.close()
