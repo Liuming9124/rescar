8bc08582d8890bd40e8ce36d16342437fc3bff36
Liuming9124 <a723479.a12@gmail.com>
2023-08-31 22:17:59 +0800

    getdataana: Bugfixed

--- web/controllers/dataanalysisctl.js
+++ web/controllers/dataanalysisctl.js
@@ -37,16 +37,14 @@ function getrevenueByYearMonth(forder) {
 function countUniqueUrlids(data) {
     const urlids = new Set();
     for (const item of data) {
-      const urlid = item.urlid;
-      urlids.add(urlid);
+        const urlid = item.urlid;
+        urlids.add(urlid);
     }
     return urlids.size;
-  }
+}
 
 async function getdatabytime(stime, etime) {
-
     try {
-
         let forder = []
         session = db.session()
         await session
@@ -66,50 +64,44 @@ async function getdatabytime(stime, etime) {
                 res.send(`{"status":"${error}"}}`)
             })
             .then(async () => {
-                var menu = []
-                menu = JSON.parse(await funCtl.getMenu())
-                //console.log('menu:',menu)
-                var food = {}; // 創建一個空物件來存儲菜單數據
-
-                for (var i = 0; i < menu.length; i++) {
-                    var category = menu[i].name; // 獲取類別名稱
-                    var items = menu[i].items;   // 獲取食物項目數組
-                    food[category] = {}; // 在food物件中創建一個空物件來存儲當前類別的食物項目
-                    for (var j = 0; j < items.length; j++) {
-                        food[category][j + 1] = { name: items[j].name, count: 0 }; // 將食物項目添加到當前類別下，注意這裡的 j + 1 作為食物項目的編號
-                    }
-                }
-
-                //   console.log('menu:',food);
-
-                // console.log(JSON.stringify(forder))   //上一層的所有訂單結果
-                // 使用第二個db連接查詢下一層訂單明細
                 var session2 = db.session()
                 const temp = forder.length
-                for (var i = 0; i < temp; i++) {
+                var i = 0
+                for await (let temp of forder) {
                     var carts = []
                     // 查詢特定類別的商品並新增至itemsarr
                     try {
-                        const results = await session2.run(`match(o) where ID(o) = ${forder[i].id} match(o) -[:orders]-> (p:cart) return p`);
-                        results.records.forEach(record => {
+                        console.log('id',temp)
+                        await session2
+                        .run(`match(o) where ID(o) = ${temp.id} match(o) -[:orders]-> (p:cart) return p`)
+                        .then(result => {
+                            result.records.forEach(record => {
                             // console.log(record.get('p').properties)
                             carts.push(record.get('p').properties);
-                        });
+                            })
+                        })
+                        .catch (error => {
+                            console.error(error);
+                        })
+                        .then(() => {
+                            // add items to forder
+                            // 為了避免直接修改原始的 forder 數組，先創建一個副本
+                            const ordersCp = forder.slice();
+
+                            // 使用 map() 方法更新 forder 數組，將新數據添加到舊數據的末尾
+                            forder = ordersCp.map((item, index) => {
+                                if (index === i) {
+                                    return { ...item, cart: carts };
+                                } else {
+                                    return item;
+                                }
+                            });
+                        })
                     } catch (error) {
                         console.error(error);
                     }
-                    // add items to forder
-                    // 為了避免直接修改原始的 forder 數組，先創建一個副本
-                    const ordersCp = forder.slice();
-
-                    // 使用 map() 方法更新 forder 數組，將新數據添加到舊數據的末尾
-                    forder = ordersCp.map((item, index) => {
-                        if (index === i) {
-                            return { ...item, cart: carts };
-                        } else {
-                            return item;
-                        }
-                    });
+                    i++
+                    console.log(i)
                 }
                 session2.close();
                 //  在此return將更新後的資料傳出
@@ -136,6 +128,57 @@ async function getdatabytime(stime, etime) {
         console.log('getDataAnalysis error:', err)
     }
 }
+async function getFormatMenu() {
+
+    // var menu = []
+    // menu = JSON.parse(await funCtl.getMenu())
+    // //console.log('menu:',menu)
+    // var food = {}; // 創建一個空物件來存儲菜單數據
+
+    // for (var i = 0; i < menu.length; i++) {
+    //     var category = menu[i].name; // 獲取類別名稱
+    //     var items = menu[i].items;   // 獲取食物項目數組
+    //     food[category] = {}; // 在food物件中創建一個空物件來存儲當前類別的食物項目
+    //     for (var j = 0; j < items.length; j++) {
+    //         food[category][j + 1] = { name: items[j].name, count: 0 }; // 將食物項目添加到當前類別下，注意這裡的 j + 1 作為食物項目的編號
+    //     }
+    // }
+
+    // //   console.log('menu:',food);
+
+    // // console.log(JSON.stringify(forder))   //上一層的所有訂單結果
+    // // 使用第二個db連接查詢下一層訂單明細
+    // var session2 = db.session()
+    // const temp = forder.length
+    // for (var i = 0; i < temp; i++) {
+    //     var carts = []
+    //     // 查詢特定類別的商品並新增至itemsarr
+    //     try {
+    //         const results = await session2.run(`match(o) where ID(o) = ${forder[i].id} match(o) -[:orders]-> (p:cart) return p`);
+    //         results.records.forEach(record => {
+    //             // console.log(record.get('p').properties)
+    //             carts.push(record.get('p').properties);
+    //         });
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    //     // add items to forder
+    //     // 為了避免直接修改原始的 forder 數組，先創建一個副本
+    //     const ordersCp = forder.slice();
+
+    //     // 使用 map() 方法更新 forder 數組，將新數據添加到舊數據的末尾
+    //     forder = ordersCp.map((item, index) => {
+    //         if (index === i) {
+    //             return { ...item, cart: carts };
+    //         } else {
+    //             return item;
+    //         }
+    //     });
+    // }
+    // session2.close();
+    // //  在此return將更新後的資料傳出
+    // return forder;
+}
 
 const dataanalysisController = {
 
@@ -157,20 +200,21 @@ const dataanalysisController = {
             // console.log(etime); // 2023-05-17-23-59-00.000Z
 
             stime = `2023-05-17-23-59-00.000Z`
-            etime = `2023-06-17-23-59-00.000Z`
+            etime = `2023-12-17-23-59-00.000Z`
 
 
             let forder = []
             forder = await getdatabytime(stime, etime);
-            console.log(forder[0])
+            // console.log(forder)
+            res.send(forder)
 
-            const revenueByYearMonth = await getrevenueByYearMonth(forder)
-            console.log(revenueByYearMonth)
-            console.log(await countUniqueUrlids(forder))
-            res.send(JSON.stringify(forder))
+            // const revenueByYearMonth = await getrevenueByYearMonth(forder)
+            // console.log(revenueByYearMonth)
+            // console.log(await countUniqueUrlids(forder))
+            // res.send(JSON.stringify(forder))
         }
         catch (err) {
-            console.log('history time error:', err)
+            console.log('getdata time error:', err)
             res.redirect('/dataanalysis')
         }
     }
