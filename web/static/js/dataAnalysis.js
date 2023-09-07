ce8152ab4b13f3166bbd9e1c994d804e422dcb98
liuming9124 <a723479.a12@gmail.com>
2023-09-07 15:48:01 +0800

    Modify: dataana, Promise

--- web/static/js/dataAnalysis.js
+++ web/static/js/dataAnalysis.js
@@ -1,77 +1,112 @@
 // 注意: timeInterval只能有以下幾種值: day, month, season, year
 function geturls(stime, etime, timeInterval) {
-    fetch(`/dataanalysis/getUrlCounts`, {
-        method: 'POST',
-        headers: {
-            'Accept': '*/*',
-            'Content-Type': 'application/json'
-        },
-        // body: JSON.stringify({ "stime": "2023-06-29", "etime": "2023-10-29", "timeInterval": "month" })
-        body: JSON.stringify({ "stime": stime, "etime": etime, "timeInterval": timeInterval })
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+    return new Promise((resolve, reject) => {
+        fetch(`/dataanalysis/getUrlCounts`, {
+            method: 'POST',
+            headers: {
+                'Accept': '*/*',
+                'Content-Type': 'application/json'
+            },
+            // body: JSON.stringify({ "stime": "2023-06-29", "etime": "2023-10-29", "timeInterval": "month" })
+            body: JSON.stringify({ "stime": stime, "etime": etime, "timeInterval": timeInterval })
         })
-        .catch(error => {
-            console.error('Error occurred:', error);
-            // Handle any errors that occurred during the fetch
-        });
+            .then(response => response.json())
+            .then(data => {
+                resolve(data);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
 }
 function getSales(stime, etime, timeInterval) {
-    fetch(`/dataanalysis/getRevenueSales`, {
-        method: 'POST',
-        headers: {
-            'Accept': '*/*',
-            'Content-Type': 'application/json'
-        },
-        // body: JSON.stringify({ "stime": "2023-06-29", "etime": "2023-10-29", "timeInterval": "month" })
-        body: JSON.stringify({ "stime": stime, "etime": etime, "timeInterval": timeInterval })
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+    return new Promise((resolve, reject) => {
+        fetch(`/dataanalysis/getRevenueSales`, {
+            method: 'POST',
+            headers: {
+                'Accept': '*/*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "stime": stime, "etime": etime, "timeInterval": timeInterval })
         })
-        .catch(error => {
-            console.error('Error occurred:', error);
-            // Handle any errors that occurred during the fetch
-        });
+            .then(response => response.json())
+            .then(data => {
+                resolve(data);
+            })
+            .catch(error => {
+                console.error('Error occurred:', error);
+                reject(error);
+            });
+    });
 }
 function getObjectSales(stime, etime, timeInterval) {
-    fetch(`/dataanalysis/getObjectSales`, {
-        method: 'POST',
-        headers: {
-            'Accept': '*/*',
-            'Content-Type': 'application/json'
-        },
-        // body: JSON.stringify({ "stime": "2023-06-29", "etime": "2023-10-29", "timeInterval": "month" })
-        body: JSON.stringify({ "stime": stime, "etime": etime, "timeInterval": timeInterval })
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+    return new Promise((resolve, reject) => {
+        fetch(`/dataanalysis/getObjectSales`, {
+            method: 'POST',
+            headers: {
+                'Accept': '*/*',
+                'Content-Type': 'application/json'
+            },
+            // body: JSON.stringify({ "stime": "2023-06-29", "etime": "2023-10-29", "timeInterval": "month" })
+            body: JSON.stringify({ "stime": stime, "etime": etime, "timeInterval": timeInterval })
         })
-        .catch(error => {
-            console.error('Error occurred:', error);
-            // Handle any errors that occurred during the fetch
-        });
+            .then(response => response.json())
+            .then(data => {
+                resolve(data);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
 }
 function getFormatMenu() {
-    fetch(`/dataanalysis/getFormatMenu`, {
-        method: 'GET',
-        headers: {
-            'Accept': '*/*',
-            'Content-Type': 'application/json'
-        },
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+    return new Promise((resolve, reject) => {
+        fetch(`/dataanalysis/getFormatMenu`, {
+            method: 'GET',
+            headers: {
+                'Accept': '*/*',
+                'Content-Type': 'application/json'
+            },
         })
-        .catch(error => {
-            console.error('Error occurred:', error);
-            // Handle any errors that occurred during the fetch
-        });
+            .then(response => response.json())
+            .then(data => {
+                resolve(data);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
 }
 
-getSales("2023-06-29", "2023-10-29", "month");
+geturls("2023-06-29", "2023-10-29", "month")
+    .then(data => {
+        console.log(data); // 处理返回的数据
+        show = data
+    })
+    .catch(error => {
+        console.error(error); // 处理错误
+    });
+getSales("2023-06-29", "2023-10-29", "month")
+    .then(data => {
+        console.log(data); // 处理返回的数据
+        show = data
+    })
+    .catch(error => {
+        console.error(error); // 处理错误
+    });
+getObjectSales("2023-06-29", "2023-10-29", "month")
+    .then(data => {
+        console.log(data); // 处理返回的数据
+        show = data
+    })
+    .catch(error => {
+        console.error(error); // 处理错误
+    });
+getFormatMenu()
+    .then(data => {
+        console.log(data); // 处理返回的数据
+        show = data
+    })
+    .catch(error => {
+        console.error(error); // 处理错误
+    });
