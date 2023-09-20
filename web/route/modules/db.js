cf08c8ee47580a000e68d8afb34aab5347889c96
Liuming9124 <a723479.a12@gmail.com>
2023-09-20 16:23:45 +0800

    ConfigSet: DBpwd in file

--- web/route/modules/db.js
+++ web/route/modules/db.js
@@ -1,9 +1,12 @@
 const neo4j = require('neo4j-driver')
+const fs = require('fs');
 
+const dbfile = JSON.parse(fs.readFileSync('config.json', 'utf8')).dbFile;
+const dbpwd  = JSON.parse(fs.readFileSync(dbfile, 'utf8')).dbpwd;
 
 const uri = 'bolt://125.229.139.63:7687';
 const user = 'neo4j';
-const password = 'O2dDsdj9AJIiKFq9rKsHd8iZAxtdo0Am7UpmMqhMhcU';
+const password = dbpwd;
 
 
 const db = neo4j.driver(uri, neo4j.auth.basic(user, password))
