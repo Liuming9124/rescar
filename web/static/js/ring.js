0e971e2c9dbf43b6fb2be5874b486ad2761308fe
Liuming9124 <a723479.a12@gmail.com>
2023-10-04 16:31:48 +0800

    Modify: robot html modify

--- web/static/js/ring.js
+++ web/static/js/ring.js
@@ -151,6 +151,11 @@ function robotStatus() {
             .catch(error => {
                 // console.log('Error occurred:', error);
                 document.getElementById('robotStatus').innerHTML = '機器人離線中';
+                document.getElementById('robotStatus').innerHTML    = "機器人狀態:none";
+                document.getElementById('cur_pos').innerHTML        = "機器人目前位置:none";
+                document.getElementById('nex_pos').innerHTML        = "機器人前往位置:none" 
+                document.getElementById('battery').innerHTML        = "電量:none"
+                document.getElementById('obstacle').innerHTML       = "順暢:none";
                 // Handle any errors that occurred during the fetch
             });
     } catch (e) {
